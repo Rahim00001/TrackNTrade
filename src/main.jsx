d182c923ea0cd8f5f8e16f5d7f486096c67d9354
Rahim00001 <rahimlpt2022@gmail.com>
2024-05-27 12:44:03 +0600

    react router and chakraUI setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
+import Main from './Layout/Main';
+import Home from './Components/Home/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
 )
